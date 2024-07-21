11a185e236e7ce01c8f7b5847d945339dfae23ee
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-07-21 19:27:38 +0530

    Gyandeep | Created mongoose model for task

---
+++ models/Task.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from 'mongoose';
+
+const TaskSchema = new mongoose.Schema({
+    userId: {
+        type: Schema.ObjectId,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    taskDeadline: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true }); //it gonna give created and updated at times
+
+export default mongoose.model("Task", TaskSchema);
