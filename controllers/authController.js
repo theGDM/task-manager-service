c2a72716223ff550ddb54693126676e27b840f86
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-07-22 10:31:34 +0530

    Gyandeep | Updated the reponse message

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ export const register = async (req, res, next) => {
         })
 
         await newUser.save();
-        res.status(200).res.status(200).json({
+        res.status(200).json({
             "success": true,
             "message": "User registered successfully!"
         });
